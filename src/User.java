d66f63ca2a1f6b24cd6ee74cd8fff8774c769cb2
Dmitro <dimamar1003@gmail.com>
2022-08-18 19:58:00 +0200

    Add deposit function

--- src/User.java
+++ src/User.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class User {
     private String UName;
-    private ArrayList arrayList;
+    private HashMap<Compare,Double> hashMap;
     private int WalletId;
 
     public  User(String name){
@@ -19,5 +20,12 @@ public class User {
     }
     public  void convert(){
 
+    }
+    public void Deposit(Compare compare, double amount){
+        if (amount < 0)
+            return;
+        if (!hashMap.containsKey(compare))
+            hashMap.put(compare,amount);
+        hashMap.put(compare,hashMap.get(compare) + amount);
     }
 }
